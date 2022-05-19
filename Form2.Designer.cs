162e1962517d64caa443b33525c29c7fb030ae65
Jed Jocson <jed.jocson@obf.ateneo.edu>
2022-05-19 21:04:17 +0800

    Add update functionality and a function to clear all textboxes.

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -29,141 +29,145 @@ namespace GRP11_Castro_Jocson_LE2
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.RecordNumberLabel = new System.Windows.Forms.Label();
-            this.RegionLabel = new System.Windows.Forms.Label();
-            this.NumberOfCasesLabel = new System.Windows.Forms.Label();
-            this.RecordNumberTextBox = new System.Windows.Forms.TextBox();
-            this.RegionTextBox = new System.Windows.Forms.TextBox();
-            this.NumberOfCasesTextBox = new System.Windows.Forms.TextBox();
-            this.AddBtn = new System.Windows.Forms.Button();
-            this.UpdateBtn = new System.Windows.Forms.Button();
-            this.DeleteBtn = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.DeleteBtn);
-            this.groupBox1.Controls.Add(this.UpdateBtn);
-            this.groupBox1.Controls.Add(this.AddBtn);
-            this.groupBox1.Controls.Add(this.NumberOfCasesTextBox);
-            this.groupBox1.Controls.Add(this.RegionTextBox);
-            this.groupBox1.Controls.Add(this.RecordNumberTextBox);
-            this.groupBox1.Controls.Add(this.NumberOfCasesLabel);
-            this.groupBox1.Controls.Add(this.RegionLabel);
-            this.groupBox1.Controls.Add(this.RecordNumberLabel);
-            this.groupBox1.Location = new System.Drawing.Point(465, 33);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(304, 382);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Functionalities";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
-            // 
-            // RecordNumberLabel
-            // 
-            this.RecordNumberLabel.AutoSize = true;
-            this.RecordNumberLabel.Location = new System.Drawing.Point(29, 49);
-            this.RecordNumberLabel.Name = "RecordNumberLabel";
-            this.RecordNumberLabel.Size = new System.Drawing.Size(82, 13);
-            this.RecordNumberLabel.TabIndex = 0;
-            this.RecordNumberLabel.Text = "Record Number";
-            // 
-            // RegionLabel
-            // 
-            this.RegionLabel.AutoSize = true;
-            this.RegionLabel.Location = new System.Drawing.Point(29, 92);
-            this.RegionLabel.Name = "RegionLabel";
-            this.RegionLabel.Size = new System.Drawing.Size(41, 13);
-            this.RegionLabel.TabIndex = 1;
-            this.RegionLabel.Text = "Region";
-            // 
-            // NumberOfCasesLabel
-            // 
-            this.NumberOfCasesLabel.AutoSize = true;
-            this.NumberOfCasesLabel.Location = new System.Drawing.Point(29, 129);
-            this.NumberOfCasesLabel.Name = "NumberOfCasesLabel";
-            this.NumberOfCasesLabel.Size = new System.Drawing.Size(88, 13);
-            this.NumberOfCasesLabel.TabIndex = 2;
-            this.NumberOfCasesLabel.Text = "Number of Cases";
-            // 
-            // RecordNumberTextBox
-            // 
-            this.RecordNumberTextBox.Location = new System.Drawing.Point(126, 46);
-            this.RecordNumberTextBox.Name = "RecordNumberTextBox";
-            this.RecordNumberTextBox.Size = new System.Drawing.Size(147, 20);
-            this.RecordNumberTextBox.TabIndex = 3;
-            // 
-            // RegionTextBox
-            // 
-            this.RegionTextBox.Location = new System.Drawing.Point(126, 89);
-            this.RegionTextBox.Name = "RegionTextBox";
-            this.RegionTextBox.Size = new System.Drawing.Size(147, 20);
-            this.RegionTextBox.TabIndex = 4;
-            // 
-            // NumberOfCasesTextBox
-            // 
-            this.NumberOfCasesTextBox.Location = new System.Drawing.Point(126, 126);
-            this.NumberOfCasesTextBox.Name = "NumberOfCasesTextBox";
-            this.NumberOfCasesTextBox.Size = new System.Drawing.Size(147, 20);
-            this.NumberOfCasesTextBox.TabIndex = 5;
-            // 
-            // AddBtn
-            // 
-            this.AddBtn.Location = new System.Drawing.Point(32, 192);
-            this.AddBtn.Name = "AddBtn";
-            this.AddBtn.Size = new System.Drawing.Size(113, 57);
-            this.AddBtn.TabIndex = 6;
-            this.AddBtn.Text = "Add";
-            this.AddBtn.UseVisualStyleBackColor = true;
-            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
-            // 
-            // UpdateBtn
-            // 
-            this.UpdateBtn.Location = new System.Drawing.Point(167, 192);
-            this.UpdateBtn.Name = "UpdateBtn";
-            this.UpdateBtn.Size = new System.Drawing.Size(106, 57);
-            this.UpdateBtn.TabIndex = 7;
-            this.UpdateBtn.Text = "Update";
-            this.UpdateBtn.UseVisualStyleBackColor = true;
-            // 
-            // DeleteBtn
-            // 
-            this.DeleteBtn.Location = new System.Drawing.Point(101, 274);
-            this.DeleteBtn.Name = "DeleteBtn";
-            this.DeleteBtn.Size = new System.Drawing.Size(112, 64);
-            this.DeleteBtn.TabIndex = 8;
-            this.DeleteBtn.Text = "Delete";
-            this.DeleteBtn.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(22, 33);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(418, 382);
-            this.dataGridView1.TabIndex = 1;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // Form2
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.BurlyWood;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.groupBox1);
-            this.Name = "Form2";
-            this.Text = "Main Window";
-            this.Load += new System.EventHandler(this.Form2_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.ResumeLayout(false);
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.DeleteBtn = new System.Windows.Forms.Button();
+      this.UpdateBtn = new System.Windows.Forms.Button();
+      this.AddBtn = new System.Windows.Forms.Button();
+      this.NumberOfCasesTextBox = new System.Windows.Forms.TextBox();
+      this.RegionTextBox = new System.Windows.Forms.TextBox();
+      this.RecordNumberTextBox = new System.Windows.Forms.TextBox();
+      this.NumberOfCasesLabel = new System.Windows.Forms.Label();
+      this.RegionLabel = new System.Windows.Forms.Label();
+      this.RecordNumberLabel = new System.Windows.Forms.Label();
+      this.dataGridView1 = new System.Windows.Forms.DataGridView();
+      this.groupBox1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.DeleteBtn);
+      this.groupBox1.Controls.Add(this.UpdateBtn);
+      this.groupBox1.Controls.Add(this.AddBtn);
+      this.groupBox1.Controls.Add(this.NumberOfCasesTextBox);
+      this.groupBox1.Controls.Add(this.RegionTextBox);
+      this.groupBox1.Controls.Add(this.RecordNumberTextBox);
+      this.groupBox1.Controls.Add(this.NumberOfCasesLabel);
+      this.groupBox1.Controls.Add(this.RegionLabel);
+      this.groupBox1.Controls.Add(this.RecordNumberLabel);
+      this.groupBox1.Location = new System.Drawing.Point(465, 33);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(304, 382);
+      this.groupBox1.TabIndex = 0;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "Functionalities";
+      this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+      // 
+      // DeleteBtn
+      // 
+      this.DeleteBtn.Location = new System.Drawing.Point(101, 274);
+      this.DeleteBtn.Name = "DeleteBtn";
+      this.DeleteBtn.Size = new System.Drawing.Size(112, 64);
+      this.DeleteBtn.TabIndex = 8;
+      this.DeleteBtn.Text = "Delete";
+      this.DeleteBtn.UseVisualStyleBackColor = true;
+      // 
+      // UpdateBtn
+      // 
+      this.UpdateBtn.Location = new System.Drawing.Point(167, 192);
+      this.UpdateBtn.Name = "UpdateBtn";
+      this.UpdateBtn.Size = new System.Drawing.Size(106, 57);
+      this.UpdateBtn.TabIndex = 7;
+      this.UpdateBtn.Text = "Update";
+      this.UpdateBtn.UseVisualStyleBackColor = true;
+      this.UpdateBtn.Click += new System.EventHandler(this.UpdateBtn_Click);
+      // 
+      // AddBtn
+      // 
+      this.AddBtn.Location = new System.Drawing.Point(32, 192);
+      this.AddBtn.Name = "AddBtn";
+      this.AddBtn.Size = new System.Drawing.Size(113, 57);
+      this.AddBtn.TabIndex = 6;
+      this.AddBtn.Text = "Add";
+      this.AddBtn.UseVisualStyleBackColor = true;
+      this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+      // 
+      // NumberOfCasesTextBox
+      // 
+      this.NumberOfCasesTextBox.Location = new System.Drawing.Point(126, 126);
+      this.NumberOfCasesTextBox.Name = "NumberOfCasesTextBox";
+      this.NumberOfCasesTextBox.Size = new System.Drawing.Size(147, 20);
+      this.NumberOfCasesTextBox.TabIndex = 5;
+      // 
+      // RegionTextBox
+      // 
+      this.RegionTextBox.Location = new System.Drawing.Point(126, 89);
+      this.RegionTextBox.Name = "RegionTextBox";
+      this.RegionTextBox.Size = new System.Drawing.Size(147, 20);
+      this.RegionTextBox.TabIndex = 4;
+      // 
+      // RecordNumberTextBox
+      // 
+      this.RecordNumberTextBox.Location = new System.Drawing.Point(126, 46);
+      this.RecordNumberTextBox.Name = "RecordNumberTextBox";
+      this.RecordNumberTextBox.Size = new System.Drawing.Size(147, 20);
+      this.RecordNumberTextBox.TabIndex = 3;
+      // 
+      // NumberOfCasesLabel
+      // 
+      this.NumberOfCasesLabel.AutoSize = true;
+      this.NumberOfCasesLabel.Location = new System.Drawing.Point(29, 129);
+      this.NumberOfCasesLabel.Name = "NumberOfCasesLabel";
+      this.NumberOfCasesLabel.Size = new System.Drawing.Size(88, 13);
+      this.NumberOfCasesLabel.TabIndex = 2;
+      this.NumberOfCasesLabel.Text = "Number of Cases";
+      // 
+      // RegionLabel
+      // 
+      this.RegionLabel.AutoSize = true;
+      this.RegionLabel.Location = new System.Drawing.Point(29, 92);
+      this.RegionLabel.Name = "RegionLabel";
+      this.RegionLabel.Size = new System.Drawing.Size(41, 13);
+      this.RegionLabel.TabIndex = 1;
+      this.RegionLabel.Text = "Region";
+      // 
+      // RecordNumberLabel
+      // 
+      this.RecordNumberLabel.AutoSize = true;
+      this.RecordNumberLabel.Location = new System.Drawing.Point(29, 49);
+      this.RecordNumberLabel.Name = "RecordNumberLabel";
+      this.RecordNumberLabel.Size = new System.Drawing.Size(82, 13);
+      this.RecordNumberLabel.TabIndex = 0;
+      this.RecordNumberLabel.Text = "Record Number";
+      // 
+      // dataGridView1
+      // 
+      this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView1.Location = new System.Drawing.Point(22, 33);
+      this.dataGridView1.MultiSelect = false;
+      this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+      this.dataGridView1.Size = new System.Drawing.Size(418, 382);
+      this.dataGridView1.TabIndex = 1;
+      this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+      this.dataGridView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDoubleClick);
+      // 
+      // Form2
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.BurlyWood;
+      this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.dataGridView1);
+      this.Controls.Add(this.groupBox1);
+      this.Name = "Form2";
+      this.Text = "Main Window";
+      this.Load += new System.EventHandler(this.Form2_Load);
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+      this.ResumeLayout(false);
 
         }
 
--- Form2.cs
+++ Form2.cs
@@ -8,51 +8,70 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace GRP11_Castro_Jocson_LE2
-{
-    public partial class Form2 : Form
-    {
-        String regionEntry;
-        int recordNumber, casesNumber;
-        DataTable tableSource = new DataTable();
+namespace GRP11_Castro_Jocson_LE2 {
+  public partial class Form2 : Form {
+    String regionEntry;
+    int recordNumber, casesNumber;
+    DataTable tableSource = new DataTable();
 
-        public Form2()
-        {
-            InitializeComponent();
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form2_Load(object sender, EventArgs e)
-        {
-            tableSource.Columns.Add("Record Number", typeof(int));
-            tableSource.Columns.Add("Region", typeof(string));
-            tableSource.Columns.Add("Cases", typeof(int));
-
-            dataGridView1.DataSource = tableSource;
-
-            tableSource.Rows.Add(1, "2", 30);
-            tableSource.Rows.Add(2, "8", 50);
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void AddBtn_Click(object sender, EventArgs e)
-        {
-            recordNumber = int.Parse(RecordNumberTextBox.Text);
-            regionEntry = RegionTextBox.Text;
-            casesNumber = int.Parse(NumberOfCasesTextBox.Text);
-            tableSource.Rows.Add(recordNumber, regionEntry, casesNumber);
-            RecordNumberTextBox.Text = "";
-            RegionTextBox.Text = "";
-            NumberOfCasesTextBox.Text = "";
-            MessageBox.Show("Add successful", "Record has been successfully added!", MessageBoxButtons.OK);
-        }
+    public Form2() {
+      InitializeComponent();
     }
+
+    private void groupBox1_Enter(object sender, EventArgs e) {
+
+    }
+
+    private void Form2_Load(object sender, EventArgs e) {
+      tableSource.Columns.Add("Record Number", typeof(int));
+      tableSource.Columns.Add("Region", typeof(string));
+      tableSource.Columns.Add("Cases", typeof(int));
+
+      dataGridView1.DataSource = tableSource;
+
+      tableSource.Rows.Add(1, "2", 30);
+      tableSource.Rows.Add(2, "8", 50);
+    }
+
+    private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+
+    }
+
+    private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e) {
+      DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+      RecordNumberTextBox.Text = selectedRow.Cells[0].Value.ToString();
+      RegionTextBox.Text = selectedRow.Cells[1].Value.ToString();
+      NumberOfCasesTextBox.Text = selectedRow.Cells[2].Value.ToString();
+    }
+
+    private void UpdateBtn_Click(object sender, EventArgs e) {
+      DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+      if (!(selectedRow is null)) {
+        recordNumber = int.Parse(RecordNumberTextBox.Text);
+        regionEntry = RegionTextBox.Text;
+        casesNumber = int.Parse(NumberOfCasesTextBox.Text);
+        selectedRow.Cells[0].Value = recordNumber;
+        selectedRow.Cells[1].Value = regionEntry;
+        selectedRow.Cells[2].Value = casesNumber;
+        MessageBox.Show("Update successful", "Record has been successfully updated!", MessageBoxButtons.OK);
+        ClearFields();
+      }
+    }
+
+    private void AddBtn_Click(object sender, EventArgs e) {
+      recordNumber = int.Parse(RecordNumberTextBox.Text);
+      regionEntry = RegionTextBox.Text;
+      casesNumber = int.Parse(NumberOfCasesTextBox.Text);
+      tableSource.Rows.Add(recordNumber, regionEntry, casesNumber);
+      MessageBox.Show("Add successful", "Record has been successfully added!", MessageBoxButtons.OK);
+      ClearFields();
+    }
+
+    private void ClearFields() {
+      RecordNumberTextBox.Clear();
+      RegionTextBox.Clear();
+      NumberOfCasesTextBox.Clear();
+    }
+  }
+
 }
