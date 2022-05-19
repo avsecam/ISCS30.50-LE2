12311f434b26ddc5dbae547f9dd1a462d44e2edc
Jed Jocson <jed.jocson@obf.ateneo.edu>
2022-05-19 18:44:47 +0800

    First commit.

---
+++ Form1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GRP11_Castro_Jocson_LE2 {
+  public partial class Form1 : Form {
+    public Form1() {
+      InitializeComponent();
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GRP11_Castro_Jocson_LE2 {
+  static class Program {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main() {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
+    }
+  }
+}
